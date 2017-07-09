82ebe12440ca5a7920fb543a640b0d9213376787
Glen Horton <hortongn@ucmail.uc.edu>
2017-07-09 02:46:04 +0000

    Initial commit

---
+++ app/src/main/java/com/glenhorton/glenculator/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.glenhorton.glenculator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        plus.setOnClickListener { toast("Answer is : ${Integer.parseInt(editText.text.toString()) + Integer.parseInt(editText2.text.toString())}") }
+        minus.setOnClickListener { toast("Answer is : ${Integer.parseInt(editText.text.toString()) - Integer.parseInt(editText2.text.toString())}") }
+        into.setOnClickListener { toast("Answer is : ${Integer.parseInt(editText.text.toString()) * Integer.parseInt(editText2.text.toString())}") }
+        divide.setOnClickListener { toast("Answer is : ${Integer.parseInt(editText.text.toString()) / Integer.parseInt(editText2.text.toString())}") }
+    }
+}
